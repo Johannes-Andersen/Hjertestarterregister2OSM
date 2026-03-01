d5906bc7575979c377e30e7626d5ad3e4d2e4caa
Johannes Andersen <johannes.andersen@telenor.no>
2026-03-01 11:51:01 +0100

    chore: increase area to østfold

--- apps/reconciler/src/utils/getOsmAeds.ts
+++ apps/reconciler/src/utils/getOsmAeds.ts
@@ -1,33 +1,37 @@
 import type { OverpassResponse } from "@repo/overpass-sdk";
 import { overpassClient } from "../clients/overpassClient.ts";
 import { overpassConfig } from "../config.ts";
-import { norwayBoundary } from "../data/norwayBoundary.ts";
 
-type PolygonRing = readonly (readonly [number, number])[];
+// import { norwayBoundary } from "../data/norwayBoundary.ts";
 
-const toOverpassPolyString = (ring: PolygonRing) =>
-  ring.map(([lon, lat]) => `${lat} ${lon}`).join(" ");
+// type PolygonRing = readonly (readonly [number, number])[];
 
-const buildDefibrillatorPolyQueryClauses = () =>
-  norwayBoundary.coordinates
-    .map((polygon) => polygon[0])
-    .filter((ring) => Array.isArray(ring) && ring.length > 2)
-    .map((ring) => ring as PolygonRing)
-    .map(
-      (ring) =>
-        `    nwr["emergency"="defibrillator"](poly:"${toOverpassPolyString(ring)}");`,
-    )
-    .join("\n");
+// const toOverpassPolyString = (ring: PolygonRing) =>
+//   ring.map(([lon, lat]) => `${lat} ${lon}`).join(" ");
+
+// const buildDefibrillatorPolyQueryClauses = () =>
+//   norwayBoundary.coordinates
+//     .map((polygon) => polygon[0])
+//     .filter((ring) => Array.isArray(ring) && ring.length > 2)
+//     .map((ring) => ring as PolygonRing)
+//     .map(
+//       (ring) =>
+//         `    nwr["emergency"="defibrillator"](poly:"${toOverpassPolyString(ring)}");`,
+//     )
+//     .join("\n");
 
 const query = `
   [out:json][timeout:${overpassConfig.queryTimeoutSeconds}];
-  // gather results for emergency=defibrillator within Norway polygon
-  (
-${buildDefibrillatorPolyQueryClauses()}
-  );
 
-  // print results
-  out geom;
+  rel
+    ["boundary"="administrative"]
+    ["admin_level"="4"]
+    ["name"="Østfold"];
+  map_to_area -> .searchArea;
+
+  nwr["emergency"="defibrillator"](area.searchArea);
+
+  out center;
 `;
 
 export const getOsmAeds = async (): Promise<OverpassResponse> =>
